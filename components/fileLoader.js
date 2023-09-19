e5a57731973e53f9fee008e4ec3c9ea2d54f2c5f
JV-DM <javiDMWork@outlook.com>
2023-09-19 14:32:42 +0200

    changed button to fit other buttons

--- components/fileLoader.js
+++ components/fileLoader.js
@@ -34,7 +34,7 @@ const FileLoader = ({ onFileUpload }) => {
         onChange={handleFileChange}
       />
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4"
+        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full"
         onClick={handleFileUpload}
       >
         Upload File
